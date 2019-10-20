5d5ad1dc443e1c470cdc50c6e19d63dd5d530f49
yoursalex <sash34@mail.ru>
2019-10-20 14:55:21 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void checkRemainIfAmountIs900() {
+        CashbackHackService cashBackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashBackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void checkRemainIfAmountIs1000() {
+        CashbackHackService cashBackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashBackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected,actual);
+    }
+}
